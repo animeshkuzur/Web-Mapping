d4d44c9c91800ae7ceb06c10f2c453b64be94c63
animeshkuzur <animesh.kuzur@outlook.com>
2016-05-04 12:11:12 +0530

    First Commit

---
+++ display.py
@@ -0,0 +1,79 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+import MySQLdb
+
+def add_nodes():
+	nodes = set()
+	conn = MySQLdb.connect("localhost","root","anni99K","xyz")
+	c = conn.cursor()
+	r = c.execute("""SELECT id FROM dump""")
+	for row in c.fetchall():
+    		nodes.add(row[0])
+
+	c.close()
+	return nodes
+
+def add_edges():
+	edges = set()
+	conn = MySQLdb.connect("localhost","root","anni99K","xyz")
+	c = conn.cursor()
+	r = c.execute("""SELECT id,from_url FROM dump""")
+	for row in c.fetchall():
+    		edges.add((row[0],row[1]))
+
+	c.close()
+	return edges
+
+nodes_set=add_nodes()
+x=0
+for x in nodes_set:
+	x+=1
+print "Number of vertices: "+str(x)
+edges_set=add_edges()
+y=0
+for a in edges_set:
+	y+=1
+print "Number of edges: "+str(y)
+#for each in nodes_set:
+#	print each
+G=nx.Graph()
+G.add_nodes_from(nodes_set)
+G.add_node(0)
+G.add_edges_from(edges_set)
+
+graph_pos=nx.random_layout(G)
+
+plt.figure(figsize=(20,20))
+nx.draw(G,graph_pos,with_labels=True,node_size=70,font_size=14)
+plt.savefig("graph3.png")
+plt.show()
+flag=True
+while flag==True:
+	print "\n\t=============================MENU=============================\n"
+	print "\tPress[1] to calculate the number of clicks to a particular Node\n"
+	print "\tPress[2] to check if two given nodes are connect\n"
+	print "\tPress[3] to quit"
+	print "\n\t==============================================================\n"
+
+	n=input("\nEnter your selection: ")
+	if n == 1:
+		node = input("\nEnter the id of the Node: ")
+		for path in nx.all_simple_paths(G, source=0,target=node):
+			print path
+			for clicks in path:
+				clicks+=1
+			print str(clicks-1)+" Clicks\n"
+			clicks=0
+	elif n ==2:
+		node1 = input("\nEnter first node: ")
+		node2 = input("\nEnter second node: ")
+		for path in nx.all_simple_paths(G, source=node1,target=node2):
+			if path:
+				print "\nThe two nodes are connected"
+				print path
+			else:
+				print "\nNot connected"
+	elif n ==3:
+		flag = False
+	else:
+		print "\nEnter a valid input"
---
+++ main2.py
@@ -0,0 +1,118 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+import MySQLdb
+import sys
+
+def query(query):
+
+	url = "https://www.bing.com/search?q="
+	for x in query:
+		url = url+x+"+"
+	url=url[0:-1]
+	url=url+"&first=00"
+	return url
+
+def search(url):
+	result=[]
+	try:
+		response_code = requests.get(url)
+		print response_code.status_code
+		if response_code.status_code == 200:
+			html = response_code.text
+			tags = BeautifulSoup(html,"lxml")
+			for hs in tags.findAll('li',attrs={'class':'b_algo'}):
+				links = hs.find('h2').find('a')
+				result.append(links['href'])			
+	except requests.ConnectionError:
+		print "no internet connection"
+	finally:
+		return result
+
+def crawl(list_q,id_no):
+
+	try:
+			z=add_to_db(list_q,id_no)
+			response_code = requests.get(list_q)
+			print response_code.status_code
+			if response_code.status_code == 200:
+				html = response_code.text
+				tags = BeautifulSoup(html,"lxml")
+				flag = 0
+				for ac in tags.findAll('a', attrs={'href':re.compile("^http")}):
+					global crawled
+					global id_from
+					#print '\n-------------------------------------------------------------------------------\n'
+					#print crawled
+					#print '\n-------------------------------------------------------------------------------\n'
+					
+					if ac['href'] not in crawled:
+						if parse(ac['href']):
+							print 'added: '+ac['href']
+							crawled.add(ac['href'])
+							#id_no=id_from
+							crawl(ac['href'],z)
+						else:
+							print 'not added: '+ac['href']
+							crawled.add(ac['href'])
+
+	except requests.ConnectionError:
+		print "no internet connection"
+	finally:
+		pass
+		
+def parse(url):
+	flag =0
+	c = False
+	if '?' not in url:
+		if '#' not in url:
+			rsp_cd = requests.get(url)
+			if rsp_cd.status_code == 200:
+				lnk_html = rsp_cd.text
+				lnk_tags = BeautifulSoup(lnk_html,"lxml")
+				for script in lnk_tags(["script", "style"]):
+   						script.extract()
+   				text = lnk_tags.get_text()
+   				text = text.encode('utf-8')
+   				txt_set = text.split(' ')
+				#print txt_set
+				global q
+				for txt_a in q:
+					for txt_b in txt_set:
+						if txt_a.lower() == txt_b.lower():
+							flag=flag+1
+				global l_q
+				if l_q<=flag:
+					c = True
+				else:
+					c= False
+	return c
+
+def add_to_db(url,from_url):
+	global id_next
+	global id_from
+	try:
+		conn = MySQLdb.connect("localhost","root","anni99K","xyz")
+		c = conn.cursor()
+		c.execute("""INSERT INTO dump (id,url,from_url,urlsha1) VALUES (%s,%s,%s,SHA1(%s))""",(id_next,url,from_url,url))
+		conn.commit()
+		c.close()
+		id_from=id_next
+		id_next+=1
+	except:
+		pass
+	return id_from
+
+id_next=1
+id_from=0
+crawled=set()
+q = sys.argv[1:]
+l_q = len(q)
+url = query(sys.argv[1:])
+print url
+y=search(url)
+for x in y:
+	print "crawling: "+x
+	#add_to_db(x,0)
+	id_from=0
+	crawl(x,id_from)
